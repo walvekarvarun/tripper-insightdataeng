055d90efc59256b9a1a5ae840660eb61805fc29c
varunwalvekar <varunwalvekar@gmail.com>
2019-06-27 11:47:25 -0700

    dropdown

--- web/app/forms.py
+++ web/app/forms.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
+import wtforms
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 class SelectForm(FlaskForm):
-    city = StringField('city', validators=[DataRequired()])
+	city = wtforms.SelectField(label='CITY', choices=('a','b'))
+    #city = StringField('city', validators=[DataRequired()])
     interests = StringField('interests', validators=[DataRequired()])
     submit = SubmitField('Submit')
